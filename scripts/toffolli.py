b8505ee62ffd810f43e7574a5b81a36decf459d2
stubbi <jannes.stubbemann@swarms.tech>
2019-12-12 13:13:20 +0100

    also test for samples and epochs

--- scripts/toffolli.py
+++ scripts/toffolli.py
@@ -7,20 +7,22 @@ import numpy as np
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            nqs = nq.nqs.NQS(3)
+            for samples in [10,100,1000,10000]:
+                for epochs in [100,1000,10000,100000,1000000,10000000]:
+                    nqs = nq.nqs.NQS(3)
 
-            # init to 000
-            nqs.applyHadamard(0,100,100000)
-            nqs.applyHadamard(1,100,100000)
-            nqs.applyHadamard(2,100,100000)
+                    # init to 000
+                    nqs.applyHadamard(0,samples,epochs)
+                    nqs.applyHadamard(1,samples,epochs)
+                    nqs.applyHadamard(2,samples,epochs)
 
-            if(x == 1):
-                nqs.applyPauliX(0)
-            if(y == 1):
-                nqs.applyPauliX(1)
-            if(z == 1):
-                nqs.applyPauliX(2)
+                    if(x == 1):
+                        nqs.applyPauliX(0)
+                    if(y == 1):
+                        nqs.applyPauliX(1)
+                    if(z == 1):
+                        nqs.applyPauliX(2)
 
-            nqs.truthTable()
-            nqs.applyToffoli(0,1,2,100,100000)
-            nqs.truthTable()
+                    nqs.truthTable()
+                    nqs.applyToffoli(0,1,2,samples,epochs)
+                    nqs.truthTable()
