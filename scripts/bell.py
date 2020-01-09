bef180ce4dc303fbcce16600595514e1351c951b
stubbi <jannes.stubbemann@swarms.tech>
2020-01-09 13:15:39 +0100

    give iterations and samples as command line arguments

--- scripts/bell.py
+++ scripts/bell.py
@@ -3,12 +3,13 @@ import nqs as nq
 import cmath
 import numpy as np
 import time
+import sys
 
 
 nqs = nq.nqs.NQS(2)
 nqs.applyControlledZRotation(0, 1, cmath.pi)
 start = time.time()
-nqs.applyHadamard(1, 100, 100000)
+nqs.applyHadamard(1, int(sys.argv[1]), int(sys.argv[2]))
 end = time.time()
 nqs.truthTable()
 print(end - start)
