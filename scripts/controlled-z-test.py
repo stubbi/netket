6f7af9ad723c6fb5d8de82b36f86f2e726bf4048
stubbi <jannes.stubbemann@swarms.tech>
2020-01-17 15:03:05 +0100

    test controlled z rotation

---
+++ scripts/controlled-z-test.py
@@ -0,0 +1,27 @@
+import nqs as nq
+import cmath
+import numpy as np
+import time
+import sys
+
+samples = int(sys.argv[1])
+epochs = int(sys.argv[2])
+initialHidden = int(sys.argv[3])
+sampleSteps = int(sys.argv[4])
+
+for x in range(2):
+    for y in range(2):
+
+        nqs = nq.nqs.NQS(2, initialHidden, sampleSteps)
+
+        nqs.applyHadamard(0,samples,epochs)
+        nqs.applyHadamard(1,samples,epochs)
+
+        if(x == 1):
+            nqs.applyPauliX(0)
+        if(y == 1):
+            nqs.applyPauliX(1)
+
+        nqs.truthTable()
+        nqs.applyControlledZRotation(0, 1, cmath.pi)
+        nqs.truthTable()
