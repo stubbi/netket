ba8568437a291521bf21c256db05368244e09d90
stubbi <jannes.stubbemann@swarms.tech>
2020-01-17 15:07:51 +0100

    test controlled z the right way

--- scripts/controlled-z-test.py
+++ scripts/controlled-z-test.py
@@ -15,13 +15,13 @@ for x in range(2):
         nqs = nq.nqs.NQS(2, initialHidden, sampleSteps)
 
         nqs.applyHadamard(0,samples,epochs)
-        nqs.applyHadamard(1,samples,epochs)
 
         if(x == 1):
             nqs.applyPauliX(0)
         if(y == 1):
-            nqs.applyPauliX(1)
+            nqs.applyPauliZ(1)
 
         nqs.truthTable()
         nqs.applyControlledZRotation(0, 1, cmath.pi)
+        nqs.applyHadamard(1,samples,epochs)
         nqs.truthTable()
