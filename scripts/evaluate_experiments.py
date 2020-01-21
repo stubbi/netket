43da5614b7fc0dd7f5650ed5d2f91abe84484fe5
stubbi <jannes.stubbemann@swarms.tech>
2020-01-21 11:29:54 +0100

    create slurm script for evaluation to have imports

---
+++ scripts/evaluate_experiments.py
@@ -0,0 +1,46 @@
+import subprocess, os, errno
+
+experiment_name = 'bell-test-qasm'
+noctua_user = 'hpc-prf-nqs'
+
+# parameters which should be evaluated
+number_of_nodes = [1]
+number_of_tasks_per_node = [1]
+number_of_omp_threads = [1]
+
+number_of_training_samples = [100]
+number_of_training_iterations = [100000]
+
+number_of_initial_hidden_units = [0]
+number_of_sample_steps = [0]
+number_of_runs = 10
+
+
+epxperiment_folder = "{pc2pfs}/{noctua_user}/{experiment_name}".format(noctua_user=noctua_user,
+                                    pc2pfs=os.environ["PC2PFS"],
+                                    experiment_name=experiment_name)
+
+
+batch_script ="""#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -J {experiment_name}-evaluation
+#SBATCH -A {noctua_user}
+#SBATCH -p {noctua_partition}
+#SBATCH -t {max_wall_time}
+#SBATCH --mail-type fail
+#SBATCH --mail-user {email}
+
+python $HOME/nqs/scripts/evaluation.py {epxperiment_folder} 2 1 1 1 100 100000 0 0 10 > out 2> err""".format(
+                        epxperiment_folder=epxperiment_folder,
+                        experiment_name=experiment_name,
+                        noctua_user=noctua_user,
+                        noctua_partition=noctua_partition,
+                        max_wall_time=max_wall_time,
+                        email=email)
+
+f = open("{epxperiment_folder}/evaluation.slurm".format(epxperiment_folder=epxperiment_folder),'w')
+print >>f, batch_script
+
+bashCommand = "sbatch -D {epxperiment_folder} {epxperiment_folder}/job.slurm".format(epxperiment_folder=epxperiment_folder)
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
