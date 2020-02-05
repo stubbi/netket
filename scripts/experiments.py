e18a90dcb0ba9db8f3d832bd154f82dea28b46ce
stubbi <jannes.stubbemann@swarms.tech>
2020-02-05 21:05:27 +0100

    new experiment

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -13,17 +13,17 @@ singularity_image_location = "{pc2pfs}/{noctua_user}/nqs.sif".format(
 # parameters to be tested
 number_of_qubits = range(2,6)
 number_of_cycles = range(2,6)
-number_of_circuits = 2 #number of random circuits with same number of qubits and cycles
+number_of_circuits = 5 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
-number_of_training_samples = [100,200]
-number_of_training_iterations = [10000,20000]
+number_of_training_samples = [100,500,1000]
+number_of_training_iterations = [10000,100000]
 
-number_of_initial_hidden_units = [0,1]
-number_of_sample_steps = [1,2]
+number_of_initial_hidden_units = [0]
+number_of_sample_steps = [0,2,4,6]
 number_of_runs = 3 #number of runs for a specific circuit
 
 
