06da1e3645bcccd3262b872f3ded117083b1b647
stubbi <jannes.stubbemann@swarms.tech>
2020-02-06 13:04:06 +0100

    see head

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -76,9 +76,8 @@ class Evaluation:
 
     def plot(self, df, groupby, y, suffix):
         fig, ax = plt.subplots()
-        print(df.columns)
         df = df.groupby(groupby, as_index = False).mean()
-        print(df.columns)
+        print(df.head())
         for u in pandas.unique(df[groupby[0]]):
             filtered = df[df[groupby[0]] == u]
             print(filtered.columns)
@@ -103,9 +102,7 @@ class Evaluation:
         if not os.path.exists('plots'):
             os.makedirs('plots')
         df = pandas.read_csv(results_file)
-        print(df.columns)
         df = df[df['success'] == True]
-        print(df.columns)
         plots(df.copy(), 'all')
 
         for i in pandas.unique(df['#iterations']):
