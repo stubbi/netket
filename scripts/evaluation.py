8a1dddbe3bb78e8514d70daf1541ef2032e8336a
stubbi <jannes.stubbemann@swarms.tech>
2020-02-10 15:36:38 +0100

    is this a list after all?

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -87,7 +87,7 @@ class Evaluation:
             name = '{}_{}_{}'.format(x, y, title.replace(' ', '_'))
             for index, row in filterBy.iterrows(): 
                 toPlot = df.merge(row.to_frame().T, 'left')
-                print(toPlot)
+                print(toPlot[x])
                 l = ''.join(['{} {} '.format(toPlot[i], i) for i in grouped])
                 ax.plot(toPlot[x], toPlot[y], label = l)
             plt.legend()
