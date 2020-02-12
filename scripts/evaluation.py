05b3fd0412f0b80fcb82751bcfef7639fd6dd18b
stubbi <jannes.stubbemann@swarms.tech>
2020-02-12 21:34:09 +0100

    print as frame

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -99,7 +99,7 @@ class Evaluation:
             fig, ax = plt.subplots()
             name = '{}_{}_{}'.format(x, y, title.replace(' ', '_'))
             for index, row in filterBy.iterrows(): 
-                print(row)
+                print(row.to_frame())
                 print(df)
                 toPlot = pandas.merge(df, row.to_frame(), how='inner')
                 l = ''.join(['{}:{} '.format(i, toPlot[i].tolist()[0]) for i in grouped])
