9c90d291dd78e4911f5641f4e64952729a689cf2
stubbi <jannes.stubbemann@swarms.tech>
2020-02-13 09:18:26 +0100

    even better naming

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -96,7 +96,7 @@ class Evaluation:
 
         for y in ['tvd', 'duration', 'f_xeb']:
             fig, ax = plt.subplots()
-            name = '{}_{}_{}'.format(x, y, title.replace(', ', '_').replace('#', ''))
+            name = '{}_{}_{}'.format(x.replace('#', ''), y.replace('#', ''), title.replace(', ', '_').replace('#', ''))
             for index, row in filterBy.iterrows(): 
                 toPlot = pandas.merge(df, row.to_frame().T, how='inner')
                 l = ''.join(['{}:{} '.format(i, toPlot[i].tolist()[0]) for i in grouped])
