925438cc7b577837562f762916c6a992e42806f7
stubbi <jannes.stubbemann@swarms.tech>
2020-02-15 13:44:38 +0100

    experiments proposal-1

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -1,7 +1,7 @@
 import subprocess, os, errno, time
 
 circuit_generator_script = 'random_circuit.py'
-experiment_name = 'new-plotting-test'
+experiment_name = 'proposal-1'
 noctua_partition = 'short'
 max_wall_time = '00:10:00'
 noctua_user = 'hpc-prf-nqs'
@@ -11,18 +11,18 @@ singularity_image_location = "{pc2pfs}/{noctua_user}/nqs.sif".format(
 
 # parameters to be tested
 number_of_qubits = [3,5,7]
-number_of_cycles = [5]
-number_of_circuits = 5 #number of random circuits with same number of qubits and cycles
+number_of_cycles = [5,7,10]
+number_of_circuits = 10 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
-number_of_training_samples = [100000] 
-number_of_training_iterations = [10000,30000,50000,70000,100000]
+number_of_training_samples = [100 + i * 200 for i in range(5)] 
+number_of_training_iterations = [10000 + i * 20000 for i in range(5)]
 
 number_of_initial_hidden_units = [0]
-number_of_sample_steps = [4,6,8] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
+number_of_sample_steps = [2,3,4,4,5,6,6,7,8] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
 number_of_runs = 1 #number of runs for a specific circuit
 
 
