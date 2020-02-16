d7422ac18ae53971e833ff18d8430d4bdbb4d88c
stubbi <jannes.stubbemann@swarms.tech>
2020-02-16 12:22:18 +0100

    more debug print

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -100,6 +100,8 @@ class Evaluation:
             for index, row in filterBy.iterrows(): 
                 toPlot = pandas.merge(df, row.to_frame().T, how='inner')
                 print(toPlot)
+                print(df)
+                print(row.to_frame().T)
                 l = ''.join(['{}:{} '.format(i, toPlot[i].tolist()[0]) for i in grouped])
                 ax.plot(toPlot[x].tolist(), toPlot[y].tolist(), label = l)
             plt.legend()
