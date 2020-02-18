b4a78ce036665e0e5c0fa6a694f24a3fb9f56b48
stubbi <jannes.stubbemann@swarms.tech>
2020-02-18 10:50:42 +0100

    fix number of sample steps

--- scripts/evaluate_experiments.py
+++ scripts/evaluate_experiments.py
@@ -18,7 +18,7 @@ number_of_training_samples = [100 + i * 200 for i in range(5)]
 number_of_training_iterations = [10000 + i * 20000 for i in range(5)]
 
 number_of_initial_hidden_units = [0]
-number_of_sample_steps = [3,5,7] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
+number_of_sample_steps = [3,5,7,9,11,13,9,11,13] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
 number_of_runs = 1 #number of runs for a specific circuit
 
 
