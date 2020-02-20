805507912375ebe4d4710eff25cb922ca8479eba
stubbi <jannes.stubbemann@swarms.tech>
2020-02-20 09:15:57 +0100

    evaluate

--- scripts/evaluate_experiments.py
+++ scripts/evaluate_experiments.py
@@ -6,9 +6,9 @@ email = 'stubbi@mail.upb.de'
 
 
 # parameters to be tested
-number_of_qubits = [3,5,7,9,11,13,15,17,19]
+number_of_qubits = range(1,24,2)
 number_of_cycles = [10]
-number_of_circuits = 8 #number of random circuits with same number of qubits and cycles
+number_of_circuits = 10 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
@@ -18,7 +18,7 @@ number_of_training_samples = [100 + i * 200 for i in range(5)]
 number_of_training_iterations = [10000 + i * 20000 for i in range(5)]
 
 number_of_initial_hidden_units = [0]
-number_of_sample_steps = [3,5,7,9,11,13,9,11,13] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
+number_of_sample_steps = number_of_qubits #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
 number_of_runs = 1 #number of runs for a specific circuit
 
 
