f103da3499f93c72044838c4b9bd788ab5d7ad59
stubbi <jannes.stubbemann@swarms.tech>
2020-03-03 15:28:09 +0100

    test training params

--- scripts/evaluate_experiments.py
+++ scripts/evaluate_experiments.py
@@ -6,16 +6,16 @@ email = 'stubbi@mail.upb.de'
 
 
 # parameters to be tested
-number_of_qubits = [5,7]
-number_of_cycles = [5,7]
+number_of_qubits = [5]
+number_of_cycles = [5]
 number_of_circuits = 10 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
-number_of_training_samples = [1000]#[100 + i * 200 for i in range(5)] 
-number_of_training_iterations = [10000]#[10000 + i * 20000 for i in range(5)]
+number_of_training_samples = [100, 500, 1000]#[100 + i * 200 for i in range(5)] 
+number_of_training_iterations = [1000, 5000, 10000]#[10000 + i * 20000 for i in range(5)]
 
 number_of_initial_hidden_units = [0]
 number_of_sample_steps = number_of_qubits #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
--- scripts/experiments.py
+++ scripts/experiments.py
@@ -10,16 +10,16 @@ singularity_image_location = "{pc2pfs}/{noctua_user}/nqs.sif".format(
                         pc2pfs=os.environ["PC2PFS"])
 
 # parameters to be tested
-number_of_qubits = [5,7]#range(3,20,2)
-number_of_cycles = [5,7] #[10]
+number_of_qubits = [5]#range(3,20,2)
+number_of_cycles = [5] #[10]
 number_of_circuits = 10 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
-number_of_training_samples = [1000]#[100 + i * 200 for i in range(5)] 
-number_of_training_iterations = [10000]#[10000 + i * 20000 for i in range(5)]
+number_of_training_samples = [100, 500, 1000]#[100 + i * 200 for i in range(5)] 
+number_of_training_iterations = [1000, 5000, 10000]#[10000 + i * 20000 for i in range(5)]
 
 number_of_initial_hidden_units = [0]
 number_of_sample_steps = number_of_qubits #[9,11,13]#[3,5,7] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
