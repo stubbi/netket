d62ac3a7e1384744a5ffc6aeb3858f206a124c9e
stubbi <jannes.stubbemann@swarms.tech>
2020-03-03 19:03:29 +0100

    dont overdo it at this stage

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,14 +1,14 @@
 # parameters to be tested
-number_of_qubits = range(2,26)
-number_of_cycles = range(10,40)
+number_of_qubits = range(2,26,2)
+number_of_cycles = range(10,41,2)
 number_of_circuits = 10 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = range(5)
 number_of_tasks_per_node = range(5)
 number_of_omp_threads = range(5)
 
-number_of_training_samples = [100 + i * 100 for i in range(50)] 
-number_of_training_iterations = [10000 + i * 10000 for i in range(50)]
+number_of_training_samples = [100 + i * 100 for i in range(5)] 
+number_of_training_iterations = [10000 + i * 10000 for i in range(5)]
 
 number_of_initial_hidden_units = [0]
 number_of_sample_steps = [q if q%2 != 0 else q+1 for q in number_of_qubits] #[9,11,13]#[3,5,7] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
