d01e9708f57a35b054a1b0d344be9ad20b478a4f
stubbi <jannes.stubbemann@gmail.com>
2020-03-06 14:01:47 +0100

    very very small exp

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -8,9 +8,9 @@ number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
-number_of_training_samples = [100 + i * 200 for i in range(5)] 
-number_of_training_iterations = [10000 + i * 20000 for i in range(5)]
+number_of_training_samples = [100 + i * 200 for i in range(2)] 
+number_of_training_iterations = [10000 + i * 20000 for i in range(2)]
 
 number_of_initial_hidden_units = [0]
 number_of_sample_steps = [q if q%2 != 0 else q+1 for q in number_of_qubits] #[9,11,13]#[3,5,7] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
-number_of_runs = 2 #number of runs for a specific circuit
+number_of_runs = 1 #number of runs for a specific circuit
