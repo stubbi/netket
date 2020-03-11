5efbe5428d41972eeff4c3f71b9a1df093e6079e
stubbi <jannes.stubbemann@gmail.com>
2020-03-11 11:09:16 +0100

    debug learning gates

---
+++ scripts/debug_circuit.py
@@ -0,0 +1,40 @@
+#program for Random Circuit creation.
+# follows algorithm from https://arxiv.org/pdf/1608.00263.pdf
+#expected file name is 'in.qc'
+import random
+import sys
+import math
+
+numQubits = int(sys.argv[1])
+numCycles = int(sys.argv[2])
+qasmFile = sys.argv[3]
+
+singleGates = ['H']
+
+root = math.floor(math.sqrt(numQubits))
+single_qubit_cycles = [['n/a' for _ in range(numQubits)]]
+def generateCycle(m):
+    cycle = 'cycle {}\n'.format(m)
+
+    for q in range(numQubits):
+        gate = random.choice(singleGates)
+        cycle = cycle + "{} q[{}]\n".format(gate, q)
+
+    cycle = cycle + '\n\n'
+
+    return cycle
+    
+
+
+# qubits will be initialized in |+> state
+circuit = """
+version 1.0
+qubits {}
+""".format(numQubits)
+
+for m in range(numCycles):
+    circuit = circuit + generateCycle(m)
+
+f = open(qasmFile,'w')
+print >>f, circuit
+f.close()
--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,8 +1,9 @@
-experiment_name = 'samplesteps'
+experiment_name = 'debug_learning'
+circuit_generator_script = 'debug_circuit.py'
 # parameters to be tested
-number_of_qubits = [10]
-number_of_cycles = [10]
-number_of_circuits = 10 #number of random circuits with same number of qubits and cycles
+number_of_qubits = [1,3,5,7,8]
+number_of_cycles = [1,2]
+number_of_circuits = 1 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
@@ -12,5 +13,5 @@ number_of_training_samples = [100 + i * 200 for i in range(5)]
 number_of_training_iterations = [10000 + i * 20000 for i in range(5)]
 
 number_of_initial_hidden_units = [0]
-number_of_sample_steps = [1,3,5,7,9,11,13,15,17]#[q if q%2 != 0 else q+1 for q in number_of_qubits] #[9,11,13]#[3,5,7] #size must be multiple of qubits (n*size), each n entries will be used for corresponding qubits
+number_of_sample_steps = [q if q%2 != 0 else q+1 for q in number_of_qubits]
 number_of_runs = 3 #number of runs for a specific circuit
