0249542a9ceafc718d17e9b2bfdfff76d318ab5a
stubbi <jannes.stubbemann@gmail.com>
2020-03-13 13:02:15 +0100

    need one hidden unit...

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -12,6 +12,6 @@ number_of_omp_threads = [1]
 number_of_training_samples = [100 + i * 400 for i in range(3)] 
 number_of_training_iterations = [10000 + i * 40000 for i in range(3)]
 
-number_of_initial_hidden_units = [0]
+number_of_initial_hidden_units = [1]
 number_of_sample_steps = [q if q%2 != 0 else q+1 for q in number_of_qubits]
 number_of_runs = 1 #number of runs for a specific circuit
