27e0807679bc11e5e2dea46f2aef0cb6d9b7a6e4
stubbi <jannes.stubbemann@gmail.com>
2020-03-13 08:54:04 +0100

    another rcs run

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,17 +1,17 @@
-experiment_name = 'debug_learning'
-circuit_generator_script = 'debug_circuit.py'
+experiment_name = 'rcs2'
+circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
-number_of_qubits = [1]
-number_of_cycles = [1,2]
-number_of_circuits = 1 #number of random circuits with same number of qubits and cycles
+number_of_qubits = [6,8]
+number_of_cycles = [8,10]
+number_of_circuits = 5 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
-number_of_training_samples = [100 + i * 200 for i in range(3)] 
-number_of_training_iterations = [10000 + i * 20000 for i in range(3)]
+number_of_training_samples = [100 + i * 400 for i in range(3)] 
+number_of_training_iterations = [10000 + i * 40000 for i in range(3)]
 
-number_of_initial_hidden_units = [1]
+number_of_initial_hidden_units = [0]
 number_of_sample_steps = [q if q%2 != 0 else q+1 for q in number_of_qubits]
 number_of_runs = 3 #number of runs for a specific circuit
