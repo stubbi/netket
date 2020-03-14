934e885d032d8464a9870cb4ec92b3a8a03d5cdc
stubbi <jannes.stubbemann@gmail.com>
2020-03-14 13:31:26 +0100

    try with less iterations

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,4 +1,4 @@
-experiment_name = 'rcsX'
+experiment_name = 'rcsIterations'
 circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
 number_of_qubits = [6]
@@ -10,7 +10,7 @@ number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
 number_of_training_samples = [100 + i * 400 for i in range(3)] 
-number_of_training_iterations = [10000 + i * 40000 for i in range(3)]
+number_of_training_iterations = [1000 + i * 4000 for i in range(3)]
 
 number_of_initial_hidden_units = [0]
 number_of_sample_steps = [q if q%2 != 0 else q+1 for q in number_of_qubits]
