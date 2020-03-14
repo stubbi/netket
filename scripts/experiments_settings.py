b61a6f060e057bdc2aa0498067ccf493b7bb6cd5
stubbi <jannes.stubbemann@gmail.com>
2020-03-14 17:11:16 +0100

    next experiment

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -10,8 +10,8 @@ number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
 number_of_training_samples = [100 + i * 400 for i in range(3)] 
-number_of_training_iterations = [1000 + i * 4000 for i in range(3)]
+number_of_training_iterations = [10000 + i * 40000 for i in range(3)]
 
 number_of_initial_hidden_units = [0]
-number_of_sample_steps = [0,1,3,5,7,9]#[q if q%2 != 0 else q+1 for q in number_of_qubits]
+number_of_sample_steps = [0,3,5,9]#[q if q%2 != 0 else q+1 for q in number_of_qubits]
 number_of_runs = 1 #number of runs for a specific circuit
