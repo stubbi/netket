a1e64f4362bdf1e9d4416a293a87848f9a49b7aa
stubbi <jannes.stubbemann@gmail.com>
2020-04-05 12:37:30 +0200

    test new CZ tactic

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,8 +1,8 @@
-experiment_name = 'rcs-16-12'
+experiment_name = 'rcs-6-8'
 circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
-number_of_qubits = [16]
-number_of_cycles = [12]
+number_of_qubits = [6]
+number_of_cycles = [8]
 number_of_circuits = 3 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
@@ -13,5 +13,5 @@ number_of_training_samples = [100 + i * 400 for i in range(3)]
 number_of_training_iterations = [10000 + i * 40000 for i in range(3)]
 
 number_of_initial_hidden_units = [0]
-number_of_sample_steps = [0,7,15]#[q if q%2 != 0 else q+1 for q in number_of_qubits]
+number_of_sample_steps = [0,3,7]#[q if q%2 != 0 else q+1 for q in number_of_qubits]
 number_of_runs = 3 #number of runs for a specific circuit
