7a40dd6df86432d43d245c3cf1a5905f4c35fb61
stubbi <jannes.stubbemann@gmail.com>
2020-04-05 12:55:37 +0200

    less runs

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -14,4 +14,4 @@ number_of_training_iterations = [10000 + i * 40000 for i in range(3)]
 
 number_of_initial_hidden_units = [0]
 number_of_sample_steps = [0,3,7]#[q if q%2 != 0 else q+1 for q in number_of_qubits]
-number_of_runs = 3 #number of runs for a specific circuit
+number_of_runs = 1 #number of runs for a specific circuit
