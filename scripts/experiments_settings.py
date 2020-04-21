88a1b9f15c72c02cd97bffb18976acb96b586b12
stubbi <jannes.stubbemann@gmail.com>
2020-04-21 12:00:07 +0200

    test rcs 6-8 again

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,9 +1,9 @@
-experiment_name = 'cz-test-bell'
-circuit_generator_script = 'bell.py'
+experiment_name = 'rcs-6-8'
+circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
 number_of_qubits = [6]
 number_of_cycles = [8]
-number_of_circuits = 1 #number of random circuits with same number of qubits and cycles
+number_of_circuits = 3 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
@@ -13,5 +13,5 @@ number_of_training_samples = [100 + i * 400 for i in range(3)]
 number_of_training_iterations = [10000 + i * 40000 for i in range(3)]
 
 number_of_initial_hidden_units = [0]
-number_of_sample_steps = [0,1]#[q if q%2 != 0 else q+1 for q in number_of_qubits]
-number_of_runs = 1 #number of runs for a specific circuit
+number_of_sample_steps = [0,3,7]#[q if q%2 != 0 else q+1 for q in number_of_qubits]
+number_of_runs = 3 #number of runs for a specific circuit
