831b31743bdf543abcb10c16cc26e1db02b12348
stubbi <jannes.stubbemann@gmail.com>
2020-04-21 09:46:13 +0200

    test cz with bell cicruit

--- scripts/bell.py
+++ scripts/bell.py
@@ -1,16 +1,21 @@
-#program for Bell state creation.
+#program for Random Circuit creation.
+# follows algorithm from https://arxiv.org/pdf/1608.00263.pdf
 #expected file name is 'in.qc'
+import random
+import sys
+import math
 
-program = """
+qasmFile = sys.argv[3]
+
+# qubits will be initialized in |+> state
+circuit = """
 version 1.0
 qubits 2
 
-# Create the Bell state with CZ gate
-H q[0:1]
 CZ q[0], q[1]
 H q[1]
 """
 
-f = open('in.qc','w')
-print >>f, program
+f = open(qasmFile,'w')
+print >>f, circuit
 f.close()
