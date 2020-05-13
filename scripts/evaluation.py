5ca5da975d79e798d0cc4bc7076d008c2b6594e8
stubbi <jannes.stubbemann@gmail.com>
2020-05-13 10:43:12 +0200

    directory

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -68,7 +68,7 @@ class Evaluation:
     def loadRBM(self, size, cycles, circuits, nodes, tasks, threads, numSamples, numIterations, numInitialHidden, numSampleSteps, run, gateNo = -1):
         f = ''
         if(gateNo == -1):
-            f = "{diretctory}/parameters.json".format(directory=self.directory(size, cycles, circuits, nodes, tasks, threads, numSamples, numIterations, numInitialHidden, numSampleSteps, run))
+            f = "{directory}/parameters.json".format(directory=self.directory(size, cycles, circuits, nodes, tasks, threads, numSamples, numIterations, numInitialHidden, numSampleSteps, run))
         else:
             f = "{directory}/parameters_gate_{gateNo}.json".format(directory=self.directory(size, cycles, circuits, nodes, tasks, threads, numSamples, numIterations, numInitialHidden, numSampleSteps, run), gateNo=gateNo)
         print(f)
