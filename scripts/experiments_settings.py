d94187f10dbbf094aabd76c7d21e1b016341dd7e
stubbi <jannes.stubbemann@gmail.com>
2020-05-23 19:33:36 +0200

    another test experiment

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,17 +1,17 @@
-experiment_name = 'learn-debug-after-each-gate-2'
+experiment_name = 'learn-debug-after-each-gate-3'
 circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
 number_of_qubits = [8]
-number_of_cycles = [1]
+number_of_cycles = [2]
 number_of_circuits = 1 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
-number_of_training_samples = [50]# + i * 400 for i in range(3)] 
-number_of_training_iterations = [50]# + i * 40000 for i in range(3)]
+number_of_training_samples = [100 + i * 400 for i in range(3)] 
+number_of_training_iterations = [10000 + i * 40000 for i in range(3)]
 
 number_of_initial_hidden_units = [0]
 number_of_sample_steps = [0] #[q if q%2 != 0 else q+1 for q in number_of_qubits]
-number_of_runs = 1 #number of runs for a specific circuit
+number_of_runs = 3 #number of runs for a specific circuit
