f520d66de587cbeed96e8db41751e4da0b622aa4
stubbi <jannes.stubbemann@gmail.com>
2020-05-28 17:24:09 +0200

    slightly bigger run for sr

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,16 +1,16 @@
-experiment_name = 'debug-sr-rcs'
+experiment_name = 'sr-1'
 circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
-number_of_qubits = [4]
-number_of_cycles = [2]
+number_of_qubits = [8]
+number_of_cycles = [6]
 number_of_circuits = 1 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
-number_of_training_samples = [100]# + i * 400 for i in range(3)] 
-number_of_training_iterations = [50000]# + i * 40000 for i in range(3)]
+number_of_training_samples = [100 + i * 400 for i in range(3)] 
+number_of_training_iterations = [50000 + i * 40000 for i in range(3)]
 
 number_of_initial_hidden_units = [0]
 number_of_sample_steps = [0] #[q if q%2 != 0 else q+1 for q in number_of_qubits]
