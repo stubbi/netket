1b9ff3a3996819a0cb2ff4f453296e848c552e25
stubbi <jannes.stubbemann@gmail.com>
2020-06-15 12:53:58 +0200

    try sr again

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,4 +1,4 @@
-experiment_name = 'AdaMax-0Restarts-NoEarlyStopping-test'
+experiment_name = 'StochasticReconfiguration-5Restarts-NoEarlyStopping-test'
 circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
 number_of_qubits = [4]
@@ -10,13 +10,13 @@ number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
 number_of_training_samples = [10 + i * 20 for i in range(3)] 
-number_of_training_iterations = [10000 + i * 20000 for i in range(3)]
+number_of_training_iterations = [1000 + i * 2000 for i in range(3)]
 
 number_of_initial_hidden_units = [int(q*(q-1)/2.0) for q in number_of_qubits]
-number_of_sample_steps = [q if q%2 != 0 else q+1 for q in number_of_qubits]
+number_of_sample_steps = [0]#q if q%2 != 0 else q+1 for q in number_of_qubits]
 number_of_runs = 1 #number of runs for a specific circuit
 
-randomRestarts = 0
+randomRestarts = 5
 earlyStopping = False
 # AdaDelta, AdaGrad, AdaMax, AMSGrad, Momentum, RMSProp, Sgd, StochasticReconfiguration
-optimizer = 'AdaMax'
+optimizer = 'StochasticReconfiguration'
