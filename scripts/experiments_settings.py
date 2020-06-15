f4c109317bd2a37702508bdd8d8416e189a72dfb
stubbi <jannes.stubbemann@gmail.com>
2020-06-15 12:23:36 +0200

    try AdaMax with more iterations and no random Restarts

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,4 +1,4 @@
-experiment_name = 'AdaMax-5Restarts-NoEarlyStopping-test'
+experiment_name = 'AdaMax-0Restarts-NoEarlyStopping-test'
 circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
 number_of_qubits = [4]
@@ -10,12 +10,13 @@ number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
 number_of_training_samples = [10 + i * 20 for i in range(3)] 
-number_of_training_iterations = [1000 + i * 2000 for i in range(3)]
+number_of_training_iterations = [10000 + i * 20000 for i in range(3)]
 
 number_of_initial_hidden_units = [int(q*(q-1)/2.0) for q in number_of_qubits]
 number_of_sample_steps = [q if q%2 != 0 else q+1 for q in number_of_qubits]
 number_of_runs = 1 #number of runs for a specific circuit
 
-randomRestarts = 5
+randomRestarts = 0
 earlyStopping = False
+# AdaDelta, AdaGrad, AdaMax, AMSGrad, Momentum, RMSProp, Sgd, StochasticReconfiguration
 optimizer = 'AdaMax'
