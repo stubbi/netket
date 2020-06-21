0a3288ef7707c85f50835d059fd03466193b90b6
stubbi <jannes.stubbemann@gmail.com>
2020-06-21 10:18:36 +0200

    smaller experiment

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -1,16 +1,16 @@
-experiment_name = 'StochasticReconfiguration-10-20-5-False'
+experiment_name = 'StochasticReconfiguration-5-10-5-False'
 circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
-number_of_qubits = [10]
-number_of_cycles = [20]
+number_of_qubits = [5]
+number_of_cycles = [10]
 number_of_circuits = 1 #number of random circuits with same number of qubits and cycles
 
-number_of_nodes = [1,4]
-number_of_tasks_per_node = [1,4]
-number_of_omp_threads = [1,4]
+number_of_nodes = [4]
+number_of_tasks_per_node = [4]
+number_of_omp_threads = [4]
 
-number_of_training_samples = [500,1000,5000,10000] 
-number_of_training_iterations = [500,1000,1500]
+number_of_training_samples = [500] 
+number_of_training_iterations = [500]
 
 number_of_initial_hidden_units = [int(q*(q-1)/2.0) for q in number_of_qubits]
 number_of_sample_steps = [0]#q if q%2 != 0 else q+1 for q in number_of_qubits]
