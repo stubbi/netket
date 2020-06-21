eeb9ce10784e8ccb6780853bbe54048efb5d5188
stubbi <jannes.stubbemann@gmail.com>
2020-06-21 10:22:41 +0200

    run more circuits

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -3,7 +3,7 @@ circuit_generator_script = 'random_circuit.py'
 # parameters to be tested
 number_of_qubits = [5]
 number_of_cycles = [10]
-number_of_circuits = 1 #number of random circuits with same number of qubits and cycles
+number_of_circuits = 3 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [4]
 number_of_tasks_per_node = [4]
