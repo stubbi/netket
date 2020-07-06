52370bded2e788da43eb1dd845f6c26a05cd157e
stubbi <jannes.stubbemann@gmail.com>
2020-07-06 11:53:05 +0200

    less experiments

--- scripts/experiments_settings.py
+++ scripts/experiments_settings.py
@@ -11,14 +11,14 @@ circuit_generator_script = 'random_circuit.py'
 number_of_qubits = [4]
 n = 4
 number_of_cycles = [5, 10, 15, 20]
-number_of_circuits = 100 #number of random circuits with same number of qubits and cycles
+number_of_circuits = 20 #number of random circuits with same number of qubits and cycles
 
 number_of_nodes = [1]
 number_of_tasks_per_node = [1]
 number_of_omp_threads = [1]
 
 
-number_of_training_samples = [samples(n), samples(n**2), samples(n**3), samples(2**n), samples(0.99 * 2**n), samples(0.98 * 2**n), samples(0.97 * 2**n), samples(0.96 * 2**n), samples(0.95* 2**n), samples(0.9 * 2**n), samples(0.85 * 2**n), samples(0.8 * 2**n)] 
+number_of_training_samples = [samples(n), samples(n**2), samples(n**3), samples(2**n), samples(0.95* 2**n), samples(0.9 * 2**n), samples(0.85 * 2**n)] 
 number_of_training_iterations = [1000, 10000, 100000]
 
 number_of_initial_hidden_units = [int(q*(q-1)/2.0) for q in number_of_qubits]
